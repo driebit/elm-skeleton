6bf7ecf8f3436ffbd26110e7de55a2b3481a1f15
rl-king <hello@rubenlieking.com>
2019-03-15 13:18:34 +0100

    Update and format test

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,4 +1,4 @@
-module Example exposing (..)
+module Example exposing (suite)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
@@ -7,4 +7,8 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
+    describe "Module name"
+        [ test "A test" <|
+            \_ ->
+                Expect.equal 1 1
+        ]
